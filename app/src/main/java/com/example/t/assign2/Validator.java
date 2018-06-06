c257f060c5b3084657e8907713b06c5cf1b2ba5a
AlanGitHubSE <zy537980@dal.ca>
2018-06-05 22:18:28 -0300

    Stage two

--- app/src/main/java/com/example/t/assign2/Validator.java
+++ app/src/main/java/com/example/t/assign2/Validator.java
@@ -15,6 +15,75 @@ public class Validator {
         if (!userPassword.equals("password"))
             rulePassed++;
 
+        // One special character
+        for (int i = 0; i < userPassword.length(); i++) {
+            boolean match = false;
+            char[] specialChar = {',', '.', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '~'};
+
+            for (int j = 0; j < specialChar.length; j++) {
+                if (userPassword.charAt(i) == specialChar[j]) {
+                    match = true;
+                    break;
+                }
+            }
+
+            if (match) {
+                rulePassed++;
+                break;
+            }
+        }
+
+        // One digit
+        for (int i = 0; i < userPassword.length(); i++) {
+            boolean match = false;
+            char[] specialChar = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+            for (int j = 0; j < specialChar.length; j++) {
+                if (userPassword.charAt(i) == specialChar[j]) {
+                    match = true;
+                    break;
+                }
+            }
+
+            if (match) {
+                rulePassed++;
+                break;
+            }
+        }
+
+        // One upper letter, one lower letter
+
+        boolean matchU = false;
+        boolean matchL = false;
+        for (int i = 0; i < userPassword.length(); i++) {
+
+            char[] specialCharUpper = {'A', 'B', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'G', 'K', 'L',
+                    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+            char[] specialCharLower = {'a', 'b', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l',
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+
+            for (int j = 0; j < specialCharUpper.length; j++) {
+                if (userPassword.charAt(i) == specialCharUpper[j]) {
+                    matchU = true;
+                    break;
+                }
+            }
+
+            for (int k = 0; k < specialCharLower.length; k++) {
+                if (userPassword.charAt(i) == specialCharLower[k]) {
+                    matchL = true;
+                    break;
+                }
+            }
+
+            if (matchU && matchL) {
+                rulePassed++;
+                break;
+            }
+        }
+
         return rulePassed;
     }
 }
--- app/src/test/java/com/example/t/assign2/JunitTest.java
+++ app/src/test/java/com/example/t/assign2/JunitTest.java
@@ -17,27 +17,62 @@ public class JunitTest {
 
     @Test
     public void Pass_One_Rule1() {
+        //Pass rule 2
         assertEquals(1, testInstance.Password_Strength("password"));
     }
 
     @Test
     public void Pass_One_Rule2() {
+        //Pass rule 1
         assertEquals(1, testInstance.Password_Strength("abcdefg"));
     }
 
     @Test
     public void Pass_One_Rules_Edge() {
+        //Pass rule 1
         assertEquals(1, testInstance.Password_Strength(""));
     }
 
     @Test
     public void Pass_Two_Rules1() {
-        assertEquals(2, testInstance.Password_Strength("StrongPassword"));
+        //Pass rule 1 & 2
+        assertEquals(2, testInstance.Password_Strength("abcdefgh"));
     }
 
     @Test
     public void Pass_Two_Rules2() {
-        assertEquals(2, testInstance.Password_Strength("Password1"));
+        //Pass rule 1 & 5
+        assertEquals(2, testInstance.Password_Strength("Uu"));
+    }
+
+    @Test
+    public void Pass_Three_Rules1() {
+        //Pass rule 1 & 2 & 3
+        assertEquals(3, testInstance.Password_Strength("password!"));
+    }
+
+    @Test
+    public void Pass_Three_Rules2() {
+        //Pass rule 1 & 2 & 4
+        assertEquals(3, testInstance.Password_Strength("password1"));
+    }
+
+    @Test
+    public void Pass_Four_Rules1() {
+        //Pass rule 1 & 2 & 3 & 4
+        assertEquals(4, testInstance.Password_Strength("abcdefg!1"));
+    }
+
+    @Test
+    public void Pass_Four_Rules2() {
+        //Pass rule 1 & 2 & 3 & 5
+        assertEquals(4, testInstance.Password_Strength("ABCdefgh!"));
+    }
+
+    @Test
+    public void Pass_Five_Rules1() {
+        //Pass rule 1 & 2 & 3 & 4 & 5
+        assertEquals(5, testInstance.Password_Strength("StrongPassword!1"));
     }
 
 }
