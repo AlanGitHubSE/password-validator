1190aa93158f4a16493b51f978333fafbe848ffe
AlanGitHubSE <zy537980@dal.ca>
2018-06-07 22:44:39 -0300

    Stage three

---
+++ app/src/androidTest/java/com/example/t/assign2/EspressoTest.java
@@ -0,0 +1,79 @@
+package com.example.t.assign2;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void InputPassword_TooWeak1() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("password"));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Too Weak")));
+    }
+
+    @Test
+    public void InputPassword_TooWeak2() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText(""));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Too Weak")));
+    }
+
+    @Test
+    public void InputPassword_Weak1() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("abcdefgh"));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Weak")));
+    }
+
+
+    @Test
+    public void InputPassword_Weak2() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("8"));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Weak")));
+    }
+
+    @Test
+    public void InputPassword_Middle() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("abcdefg8"));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Middle")));
+    }
+
+    @Test
+    public void InputPassword_Strong() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("strong!1."));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Strong")));
+    }
+
+    @Test
+    public void InputPassword_Very_Strong1() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("V.Strong!1."));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Very Strong")));
+    }
+
+    @Test
+    public void InputPassword_Very_Strong2() {
+        Espresso.onView(withId(R.id.password_Field)).perform(typeText("StrongPassword1!"));
+        Espresso.onView(withId(R.id.validate_Button)).perform(click());
+        Espresso.onView(withId(R.id.strength_Indicator)).check(matches(withText("Very Strong")));
+    }
+}
--- app/src/main/java/com/example/t/assign2/MainActivity.java
+++ app/src/main/java/com/example/t/assign2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.t.assign2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ClickButton(View view){
+
+        EditText passwordField = (EditText)findViewById(R.id.password_Field);
+        TextView strengthIndicator = (TextView)findViewById(R.id.strength_Indicator);
+
+        Validator validate = new Validator();
+       int rulePassed = validate.Password_Strength(passwordField.getText().toString());
+
+        if(rulePassed == 1){
+            strengthIndicator.setText("Too Weak");
+        }
+        else if(rulePassed ==2){
+            strengthIndicator.setText("Weak");
+        }
+        else if(rulePassed ==3){
+            strengthIndicator.setText("Middle");
+        }
+        else if(rulePassed ==4){
+            strengthIndicator.setText("Strong");
+        }
+        else if(rulePassed ==5){
+            strengthIndicator.setText("Very Strong");
+        }
+        else{
+            strengthIndicator.setText("Error occurred, # of rule passed: "+ rulePassed);
+        }
     }
 }
